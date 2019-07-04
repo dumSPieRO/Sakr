3cf69fd0ab1a3faf7d8440c414ba0924733a0399
Сяргей Адамовіч <sergey211998i@gmail.com>
2019-07-04 13:30:26 +0300

    First push

---
+++ Sakr/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sakr
+{
+    //Dev
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! developer");
+        }
+    }
+}
