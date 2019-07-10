6e4fad6c461043b96955c2bc77a2ad758c176e4b
Сяргей Адамовіч <sergey211998i@gmail.com>
2019-07-10 17:16:40 +0300

    qw

--- Sakr/Program.cs
+++ Sakr/Program.cs
@@ -9,8 +9,66 @@ namespace Sakr
         {
             Console.WriteLine("Hello World! developer");
             Console.WriteLine("Git");
-            231
-
         }
     }
+
+    interface Iadder
+    {
+        int fweight();
+        int fvolume();
+        int inventory();
+    }
+
+
+    interface ITransport: Iadder
+    {
+        string name;
+        int weight;
+        int volume;
+
+        int fweight();
+        int fvolume();
+        int inventory();
+    }
+
+    class Transport : ITransport
+    {
+        public string name{get; set;}
+        public int weight{get; set;}
+        public int volume{get; set;}
+
+        public int fweight()
+        {
+
+        }
+        public int fvolume()
+        {
+
+        }
+        public int inventory()
+        {
+
+        }
+
+        public void charge(int n, string name)
+        {
+
+        }
+        public void charge(string name)
+        {
+
+        }
+
+        public void discharge(int n, string name)
+        {
+
+        }
+
+        public bool limit()
+        {
+            return true;
+        }
+
+    }
+
 }
